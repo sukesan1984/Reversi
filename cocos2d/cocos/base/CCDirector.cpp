56b0290ef882167fbfd423a50125e448b188594c
sukesan1984 <takamikousuke@gmail.com>
2015-04-18 21:49:21 +0900

    bannerの位置だけ、FPSの表示をずらす

--- cocos2d/cocos/base/CCDirector.cpp
+++ cocos2d/cocos/base/CCDirector.cpp
@@ -1252,9 +1252,13 @@ void Director::createStatsLabel()
     Texture2D::setDefaultAlphaPixelFormat(currentFormat);
 
     const int height_spacing = 22 / CC_CONTENT_SCALE_FACTOR();
-    _drawnVerticesLabel->setPosition(Vec2(0, height_spacing*2) + CC_DIRECTOR_STATS_POSITION);
-    _drawnBatchesLabel->setPosition(Vec2(0, height_spacing*1) + CC_DIRECTOR_STATS_POSITION);
-    _FPSLabel->setPosition(Vec2(0, height_spacing*0)+CC_DIRECTOR_STATS_POSITION);
+    const int banner_height = 50;
+    //_drawnVerticesLabel->setPosition(Vec2(this->getVisibleOrigin().x, this->getVisibleOrigin().y + height_spacing*2 + banner_height) + CC_DIRECTOR_STATS_POSITION);
+    _drawnVerticesLabel->setPosition(Vec2(0, height_spacing*2 + banner_height) + CC_DIRECTOR_STATS_POSITION);
+    //_drawnBatchesLabel->setPosition(Vec2(this->getVisibleOrigin().x, this->getVisibleOrigin().y + height_spacing*1 + banner_height) + CC_DIRECTOR_STATS_POSITION);
+    _drawnBatchesLabel->setPosition(Vec2(0, height_spacing*1 + banner_height) + CC_DIRECTOR_STATS_POSITION);
+    //_FPSLabel->setPosition(Vec2(0, height_spacing*0)+CC_DIRECTOR_STATS_POSITION);
+    _FPSLabel->setPosition(Vec2(0, height_spacing*0 + banner_height)+CC_DIRECTOR_STATS_POSITION);
 }
 
 void Director::setContentScaleFactor(float scaleFactor)
