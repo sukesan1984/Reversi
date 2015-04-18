5d163d17102d7769840ec9eba76e7d4516de9b01
sukesan1984 <takamikousuke@gmail.com>
2015-04-18 20:17:57 +0900

    remove unused code

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -54,20 +54,10 @@ bool HelloWorld::init()
     // add a label shows "Hello World"
     // create and initialize a label
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
     SpriteFrameCache::getInstance()->addSpriteFramesWithFile("textures.plist");
     
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::createWithSpriteFrameName("background.png");
-    //auto sprite = Sprite::create("sample.png");
+    Sprite *sprite = Sprite::createWithSpriteFrameName("background.png");
 
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
