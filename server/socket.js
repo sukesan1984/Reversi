01ce3b62e7e1249a2c5e866a3b022cfad87f5747
sukesan1984 <takamikousuke@gmail.com>
2015-05-17 16:33:02 +0900

    serverコード

--- server/socket.js
+++ server/socket.js
@@ -1,6 +1,14 @@
 server = require('http').Server();
+var app = require('express')();
+var http = require('http').Server(app);
+
 var socketIO = require('socket.io');
 var io = socketIO.listen(server);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
 io.sockets.on('connection', function(socket){
   console.log("connect");
   io.sockets.emit('hello', 'hoge');
@@ -8,8 +16,15 @@ io.sockets.on('connection', function(socket){
       console.log("hello : " + data.value);
       io.sockets.emit('hello', { value: data.value });
   });
+
+  socket.on('chat message', function(msg){
+      console.log('message: ' + msg.value);
+      io.emit('chat message', msg);
+  });
+
   socket.on('disconnect', function(){
       console.log("disconnect");
   });
 });
+
 server.listen(3000);
